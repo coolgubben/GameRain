a368ee639d72a7e6e1684174d696982671ffb6f2
CoolGubben <hehejj@hotmail.se>
2014-10-08 10:07:32 +0200

    första här är det vi utgår från

---
+++ Game2DProject1/Assets/ParaplyCollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParaplyCollision : MonoBehaviour {
+
+	public Transform body;
+	public Transform paraply;
+	void Start() {
+		Physics2D.IgnoreCollision(paraply.collider2D, collider2D);
+	}
+}
