dfa765abdfe48940af039c8a78d26d26cb588a49
CoolGubben <hehejj@hotmail.se>
2014-10-13 10:28:44 +0200

    Fixade så paraplyet aöötid perkar mot musen och att den åker mot den också - Erik

---
+++ Game2DProject1/Assets/FollowMouse.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 objpos = Player.position; //Object position on screen
+
+		Vector2 relobjpos = new Vector2(objpos.x,objpos.y);
+
+		Debug.Log ("relpos x" + relobjpos.x);
+		Debug.Log ("relpos y" + relobjpos.y);
+
+		Vector2 mouse = Camera.main.ScreenToViewportPoint(Input.mousePosition); 
+
+		Vector2 relmousepos = new Vector2 ((mouse.x - 0.5f) * 2,(mouse.y - 0.5f) * 2) + relobjpos;
+
+		Debug.Log ("mouse x " + relmousepos.x);
+		Debug.Log ("mouse y " + relmousepos.y);
+
+		transform.position = Vector3.Lerp (relobjpos, relmousepos, 10);
+	}
+}
---
+++ Game2DProject1/Assets/MoveTowardsMouse.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsMouse : MonoBehaviour {
+	public Transform startMarker;
+	//public Transform endMarker;
+	public float speed = 1.0F;
+	private float startTime;
+	private float journeyLength;
+	public Transform target;
+	public float smooth = 5.0F;
+	void Start() {
+
+	}
+	void Update() {
+		startTime = Time.time;
+		Vector2 relmousepos = Camera.main.ScreenToViewportPoint(Input.mousePosition);        //Mouse position
+		Vector3 relobjpos = Camera.main.WorldToViewportPoint (startMarker.position);	//Object position on screen
+
+		Debug.Log (startMarker.position.x);
+		//Vector2 relobjpos = new Vector2(objpos.x - 0.5f,objpos.y - 0.5f);           //Set coordinates relative to object
+		//Vector2 relmousepos = new Vector2 (mouse.x - 0.5f,mouse.y - 0.5f) - relobjpos;
+
+
+		journeyLength = Vector3.Distance(relobjpos, relmousepos);
+		float distCovered = (Time.time - startTime) * speed;
+		float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp(relobjpos, relmousepos, fracJourney);
+	}
+}
